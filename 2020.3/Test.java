9a708a376e0f46a395b375d8cea6b8e2202f8529
maxkim77 <141907655+maxkim77@users.noreply.github.com>
2024-03-24 00:00:00 +0900

    :sparkles: update

--- 2020.3/Test.java
+++ 2020.3/Test.java
@@ -1,10 +1,10 @@
-public class Test {
-    public static void main(String []args){
+public class Test { // Test 클래스 선언
+    public static void main(String []args){ // main 메소드 선언
         int i = 0;
         int sum = 0;
         while(i < 10) {
-            i++;
-            if(i%2==1)
+            i++; // i를 1씩 증가시킴
+            if(i%2==1) // 홀수인지
             continue;
             // 홀수이면 continue로 아래 코드를 실행하지 않고 다음 반복으로 넘어감
         sum += i; // 짝수일 때만 더함
