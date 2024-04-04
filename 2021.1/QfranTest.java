e0aa0a6a2de54e3d839fd31d5fff04d988e3395a
maxkim77 <141907655+maxkim77@users.noreply.github.com>
2024-04-04 12:00:00 +0900

    :sparkles: update

---
+++ 2021.1/QfranTest.java
@@ -0,0 +1,10 @@
+public class QfranTest{
+    public static void main(String []args){
+       int a[][] = {{45, 50, 75}, {89}};
+       System.out.println(a[0].length);
+       System.out.println(a[1].length);
+       System.out.println(a[0][0]);
+       System.out.println(a[0][1]);
+       System.out.println(a[1][0]);
+    }
+}
