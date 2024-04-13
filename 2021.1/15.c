28993d9b6a1ba3cc3f58ceba7b285c8632e0711b
maxkim77 <141907655+maxkim77@users.noreply.github.com>
2024-04-13 17:05:06 +0900

    :sparkles: update

--- 2021.1/15.c
+++ 2021.1/15.c
@@ -1,16 +1,16 @@
-#include 
+#include <stdio.h>
 
 struct good {
     char name[10];
     int age;
  };
  
- void main(){
+int main() {
  	struct good s[] = {"Kim",28,"Lee",38,"Seo",50,"Park",35};
     
     struct good *p;
     p = s;
     p++;
     printf("%sn", p-> name);
-    printf("%sn", p-> age);
+printf("%d\n", p->age);
 }
