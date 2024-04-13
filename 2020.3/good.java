02b640a3b7d55aaf701bbad9109c025a3723759b
maxkim77 <141907655+maxkim77@users.noreply.github.com>
2024-04-13 15:06:37 +0900

    :sparkles: update

---
+++ 2020.3/good.java
@@ -0,0 +1,33 @@
+abstract class Vehicle {
+    private String name;
+    
+    public String getName(String val) {
+        return "Vehicle name: " + val;
+    }
+    
+    public String getName() {
+        return "Vehicle name: " + name;
+    }
+    
+    public void setName(String val) {
+        name = val;
+    }
+}
+
+class Car extends Vehicle {
+    public Car(String val) {
+        setName(val);
+    }
+
+    @Override
+    public String getName() {
+        return "Car name: " + super.getName();
+    }
+}
+
+public class Good {
+    public static void main(String[] args) {
+        Vehicle obj = new Car("Spark");
+        System.out.print(obj.getName());
+    }
+}
