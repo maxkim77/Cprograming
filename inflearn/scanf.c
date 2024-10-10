ee2dfa9c2a6236ea42316618000280bdd761ab07
maxkim77 <141907655+maxkim77@users.noreply.github.com>
2024-10-10 12:00:00 +0900

    :sparkles:update

--- inflearn/scanf.c
+++ inflearn/scanf.c
@@ -5,5 +5,13 @@ int main(void){
     scanf("%d", &input);
     printf("입력값: %d\n",input);
 
-    return 0;
+
+    int one, two, three;
+    printf("3개의 정수를 입력하세요 ");
+    scanf("%d %d %d", &one, &two, &three);
+    printf("첫번째 값  %d\n", one);
+    printf("첫번째 값  %d\n", two);
+    printf("첫번째 값  %d\n", three);
+        return 0;
+
 }
