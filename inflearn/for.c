ed26f2208afc59f3c133130b55c84f7cbce3fe28
maxkim77 <141907655+maxkim77@users.noreply.github.com>
2024-10-19 12:00:00 +0900

    :sparkles:update

--- inflearn/for.c
+++ inflearn/for.c
@@ -4,5 +4,11 @@ int main(void){
     for(int i =1; i<=10;i++){
         printf("hello world %d\n",i);
     }
+    for(int i=1;i<=3;i++){
+        printf("첫번째 반복문:%d\n",i);
+        for(int j=1; j<=5;j++){
+            printf("두번째 반복문:%d\n",j);
+        }
+    }
     return 0;
 }
