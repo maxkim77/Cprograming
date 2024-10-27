4a46d0ad70c19b1a7072b90ce730333393e8fde4
maxkim77 <141907655+maxkim77@users.noreply.github.com>
2024-10-27 12:00:00 +0900

    :sparkles:update

--- inflearn/condition.c
+++ inflearn/condition.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
 int main(void){
     int age = 25;
-    if(age>=20){
-        printf("일반인 입니다.\n");
+    // if(age>=20){
+    //     printf("일반인 입니다.\n");
+    // }else{
+    //     printf("학생입니다.\n");
+    // }
+    if(age>=8 && age<=13){
+        printf("초등학생입니다.\n");
+    }else if(age>=14 && age<=16){
+        printf("중학생입니다.\n");
+    }else if(age > 17 && age<=19){
+        printf("고등학생입니다.\n");
     }else{
-        printf("학생입니다.\n");
+        printf("학생이 아닌가봐요.\n");
     }
     return 0;
 }
