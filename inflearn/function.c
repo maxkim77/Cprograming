f0d41e0508363dbfcc3e4e64d552bff8ae7bdea2
maxkim77 <141907655+maxkim77@users.noreply.github.com>
2024-11-20 12:00:00 +0900

    :sparkles:update

--- inflearn/function.c
+++ inflearn/function.c
@@ -2,5 +2,14 @@
 int main(void){
 
     int num = 2;
-    printf("ㅜㅕ")
+    printf("num 은 %d 입니다 \n");
+
+    num = num+3;
+    printf("num은 %d입니다. \n");
+
+    num -=1;
+    printf("num은 %d입니다. \n");
+
+    num *=3;
+    printf("num은 %d입니다. \n");
 }
