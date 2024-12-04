6e6714f0d811acb0af62ee4c3c2aa52ab5a90082
maxkim77 <141907655+maxkim77@users.noreply.github.com>
2024-12-04 12:00:00 +0900

    :sparkles:update

---
+++ inflearn/f2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <time.h>
+
+int getRandomNumber(int level);
+
+int main(void){
+    
+    // 눈이 5개가 있다고 가정 , 각 문마다 점점 어려운 수식퀴즈가 출제됩니다.
+    //문제 맞히면 통과, 틀리면 실패
+
+    srand(time(NULL));
+    int count = 0;
+    for(int i = 1; i<5;i++){
+        int num1 = getRandomNumber(i);
+        int num2 = getRandomNumber(i);
+        showQuestion(i, num1, num2);
+    }
+    return 0;
+}
