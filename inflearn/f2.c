f9f724dde498d9f35ba5dc1b943cbc75eb8b20f8
maxkim77 <141907655+maxkim77@users.noreply.github.com>
2024-12-09 12:00:00 +0900

    :sparkles:update

--- inflearn/f2.c
+++ inflearn/f2.c
@@ -1,37 +1,58 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 
 int getRandomNumber(int level);
+void showQuestion(int level, int num1, int num2);
+void success();
+void fail();
 
 int main(void){
-    
-    // 눈이 5개가 있다고 가정 , 각 문마다 점점 어려운 수식퀴즈가 출제됩니다.
-    //문제 맞히면 통과, 틀리면 실패
-
-    srand(time(NULL));
+    srand(time(NULL)); // Initialize random number generator
     int count = 0;
-    for(int i = 1; i<5;i++){
+    
+    // Loop for 4 levels of questions
+    for(int i = 1; i <= 4; i++){
         int num1 = getRandomNumber(i);
         int num2 = getRandomNumber(i);
+        
+        // Show the question
         showQuestion(i, num1, num2);
+        
         int answer = -1;
-        scanf("5d", &answer);
+        scanf("%d", &answer); // Correct scanf format specifier
+        
         if(answer == -1){
-            printf("프로그램을 종료합니다.");
-            break;
+            printf("프로그램을 종료합니다.\n");
+            return 0; // Exit program gracefully
+        } else if(answer == num1 * num2){
+            success();
+            count++;
+        } else {
+            fail();
         }
     }
+
+    // After 4 questions, show the total score
+    printf("총 %d문제를 맞췄습니다!\n", count);
     return 0;
 }
 
-int getRandomNumber(int Level){
-    return rand() % (level * 7) +1;
+int getRandomNumber(int level){
+    return rand() % (level * 7) + 1;
 }
 
 void showQuestion(int level, int num1, int num2){
-  printf("\n\n\n### %d번째 비밀번호 ####\n", level);
-  printf("\n\t%d*%d?\n",num1,mum2);
-  printf("#######\n");
-  printf("\n비밀번호를 입력하세요 (종료:-1) >>");
+    printf("\n\n\n### %d번째 비밀번호 ####\n", level);
+    printf("\n\t%d * %d = ?\n", num1, num2);
+    printf("#######\n");
+    printf("\n비밀번호를 입력하세요 (종료:-1) >> ");
+}
 
+void success(){
+    printf("정답입니다! \n");
+}
+
+void fail(){
+    printf("틀렸습니다. 다시 시도하세요.\n");
 }
