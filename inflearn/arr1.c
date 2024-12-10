9275cbbd237052318b5d424802917d0c58e5c6b3
maxkim77 <141907655+maxkim77@users.noreply.github.com>
2024-12-10 12:00:00 +0900

    :sparkles:update

---
+++ inflearn/arr1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void){
+    int subway_1 = 30;
+    int subway_2 = 40;
+    int subway_3 = 50;
+    printf("지하철은 %d명이 타고있습니다.",subway_1);
+    printf("지하철은 %d명이 타고있습니다.",subway_2);
+    printf("지하철은 %d명이 타고있습니다.",subway_3);
+    return 0;
+}
